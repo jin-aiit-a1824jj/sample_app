57005d0eac3801b3b3e51218506425fee1cb7907
git-test <git.test@git.test>
2018-05-02 02:13:31 +0000

    get User/1, image gravatar_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  
+  def show
+    # 変数 ーローカル変数変数
+    # @変数 ーインスタンス変数
+    @user = User.find(params[:id])
+    #@user = User.find_by(params[:id])
+    # @user => app/views/users/show.html.erb
+    # debugger #break point, next step exit 使用しない時は解除！
+  end
+
   def new
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  #get 'users/new'
 
   root 'static_pages#home' # => root_path
   
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  
+  resources :users
   
   # => contact_path
   
