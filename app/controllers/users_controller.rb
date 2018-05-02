c321907ee1b2c9d28dc70417e4a26d46eebde9ce
git-test <git.test@git.test>
2018-05-02 02:53:08 +0000

    parameter secure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,32 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    #@user = User.new
+    #@user.name  = params[:user][:name]
+    #@user.email = params[:user][:email]
+    #              params[:user][:password]
+    
+    #@user = User.new(params[:user]) #params[:user] <- secureではない ここのparamsはユーザーが入力する部分
+    
+    @user =  User.new(user_params)
+    
+    if @user.save # => validation
+      #success
+    else
+      #failures
+      render 'new'
+    end
+    
+    
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
