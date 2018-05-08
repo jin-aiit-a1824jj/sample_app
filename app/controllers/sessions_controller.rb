0aa65efc683f1fdf0ad975d30e15e15db00810f7
git-test <git.test@git.test>
2018-05-08 02:09:09 +0000

    8.1 session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  def new
+    #@session = Session.new  modelがないので不可
+  end
+
+
+  #POST /login => create action
+  def create
+    user = User.find_by(email: params[:session][:email].downcase) 
+    #find_by 見つからない場合=＞nill
+    #nil objectでは、authenticateメッソドが定義されているわけがない メッソドが定義されてないエラーがでる。
+    #nil -> false
+    if user && user.authenticate(params[:session][:password]) #User object or false
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      
+    else
+      # エラーメッセージを作成する
+      #flash[:danger] = 'Invaild email/password combination'
+      #flash 次のリクエストがくるまで生きている flash.nowを使う
+      flash.now[:danger] = 'Invaild email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+
+
+end
