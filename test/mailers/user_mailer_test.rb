b968c318f8f65d9366d99168bcabffb4aac82f6c
git-test <git.test@git.test>
2018-05-11 02:00:53 +0000

    add test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -31,4 +31,16 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
 
+  #リスト 12.12: パスワード再設定用メイラーメソッドのテストを追加する
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
+  end
+
 end
