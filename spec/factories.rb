f3c423c4291a8396e978c5e79f72bb6d1849328c
git-test <git.test@git.test>
2018-05-31 02:18:21 +0000

    micropost_spec 追加

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 
 FactoryBot.define do
-  
+  #User_spec
   factory :michael, class: User do
     
     name "Michael Example"
@@ -28,5 +28,13 @@ FactoryBot.define do
     activated true
     activated_at Time.zone.now
   end
+  
+  #Micropost_spec
+   factory :most_recent, class: Micropost do
+    
+    content "Writing a short test"
+    created_at Time.zone.now
+    user User.first
+   end
   
 end
---
+++ spec/micropost_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'micropost' do
+
+  before do
+   @user = create(:michael)
+   @micropost = @user.microposts.build(content: "Lorem ipsum")
+  end
+
+  it "should be valid" do
+    expect(@micropost.valid?).to eq true
+  end
+
+  it "user id should be present" do
+    @micropost.user_id = nil
+    expect(@micropost.valid?).to eq false
+  end
+  
+  #リスト 13.7: Micropostモデルのバリデーションに対するテスト
+  
+  it "content should be present" do
+    @micropost.content = "   "
+    expect(@micropost.valid?).to eq false;
+  end
+
+  it "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    expect(@micropost.valid?).to eq false;
+  end
+  
+  #リスト 13.14: マイクロポストの順序付けをテストする
+  it "order should be most recent first" do
+    #assert_equal create(:most_recent), Micropost.first
+    expect(create(:most_recent)).to eq Micropost.first
+  end
+
+  
+end
