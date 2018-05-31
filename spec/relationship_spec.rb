d6dc75b5ed93ca97225b4756c9f2b9e5c1bbf9c5
git-test <git.test@git.test>
2018-05-31 02:23:19 +0000

    relationship_spec ADD

---
+++ spec/relationship_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'relationship' do
+
+  before do
+    
+   @relationship = Relationship.new(follower_id: create(:michael).id,
+                                     followed_id: create(:archer).id)
+  end
+
+  it "should be valid" do
+    expect(@relationship.valid?).to eq true
+  end
+
+  it "should require a follower_id" do
+    @relationship.follower_id = nil
+    expect(@relationship.valid?).to eq false
+  end
+
+  it "should require a followed_id" do
+    @relationship.followed_id = nil
+    expect(@relationship.valid?).to eq false
+  end
+
+  
+end
