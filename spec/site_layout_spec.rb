9046c18a3bcee3c063337101d769825b16673849
git-test <git.test@git.test>
2018-06-01 01:39:46 +0000

    site layout test ADD

---
+++ spec/site_layout_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe "site_layout_Test" , :type => :request do
+
+  it "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    
+  end
+
+end
