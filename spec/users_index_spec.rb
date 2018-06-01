2161fa293e51e5f472e88361a9ccf8814dbffe50
git-test <git.test@git.test>
2018-06-01 01:26:52 +0000

    user_index_test ADD

---
+++ spec/users_index_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'rails_helper'
+require 'login_helper'
+
+describe "Users_index_Test" , :type => :request do
+
+  include LoginModule
+  
+  before do
+    #@user = create(:michael)
+    @admin     = create(:michael)
+    @non_admin = create(:archer)
+  end
+
+  it "index including pagination" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+  end
+
+  it "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+
+    before_user_count = User.count
+    delete user_path(@non_admin)
+    after_user_count = User.count
+    
+    expect( after_user_count - before_user_count ).to eq -1
+  
+  end
+
+  it "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+end
+
